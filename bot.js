1e3267a5dcb99611f97d88fff0e46e1851365a4f
Jeremy Leitmeyer <jeremyleitmeyer@Jeremys-MacBook-Air.local>
2018-01-08 10:19:15 -0500

    mixed in l00d-bot

--- bot.js
+++ bot.js
@@ -1,106 +1,150 @@
-const Discord = require('discord.io');
-const logger = require('winston');
-const bodyParser = require('body-parser');
-const reg = require('./regex-weburl.js');
+const Discord = require('discord.io')
+const logger = require('winston')
+const bodyParser = require('body-parser')
+const reg = require('./regex-weburl.js')
 
 // global
-var msg, re_weburl;
+var msg, re_weburl
 
 // default Playing text
 var activity = 'with sauce'
 
 // variables for eventual api call
-var lastMsg = '';
-var objData = {};
+var lastMsg = ''
+var objData = {}
 
-//logger settings
-logger.remove(logger.transports.Console);
+// logger settings
+logger.remove(logger.transports.Console)
 
 logger.add(logger.transports.Console, {
-	colorize: true
-});
+  colorize: true
+})
 
-logger.level = 'debug';
+logger.level = 'debug'
 
 var bot = new Discord.Client({
-	token: process.env.BOT_TOKEN,
-	autorun: true
-});
+  token: process.env.BOT_TOKEN,
+  autorun: true
+})
 
 bot.on('ready', function (evt) {
-	logger.info('Connected');
-	logger.info('Logged in as: ');
-	logger.info(bot.username + ' - (' + bot.id + ')');
-	bot.setPresence({
-		game: {
-			name: activity
-		}
-	});
-});
+  logger.info('Connected')
+  logger.info('Logged in as: ')
+  logger.info(bot.username + ' - (' + bot.id + ')')
+  bot.setPresence({
+    game: {
+      name: activity
+    }
+  })
+})
 
 // import commands file
-const command = require('./command.js');
+const command = require('./command.js')
 
-function isURL(msg) {
-	if (reg.re_weburl.test(msg) === false) {
-		return false
-	} else {
-		// clear and push single URL into lastMessage
-		lastMsg = msg
-		return true
-	};
+function isURL (msg) {
+  if (reg.re_weburl.test(msg) === false) {
+    return false
+  } else {
+    // clear and push single URL into lastMessage
+    lastMsg = msg
+    return true
+  }
 }
 
-function setID(channelID) {
-	if (isURL(msg)) {
-		objData[channelID] = lastMsg
-	}
+function setID (channelID) {
+  if (isURL(msg)) {
+    objData[channelID] = lastMsg
+  }
 }
 
 bot.on('message', function (user, userID, channelID, message, evt) {
+  // fun :P
+  if (message === 'this bot sucks') {
+    bot.sendMessage({
+      to: channelID,
+      message: 'I am sorry for my incompetence User-sama...   *bow*'
+    })
+  }
 
-	// decides whether the data is an upload or a URL message
-	if (message === '' && evt.d.author.bot != true) {
-		// if we get an upload, we look for the attachment URL
-		msg = evt.d.attachments[0].url
-		isURL(msg)
-		setID(channelID)
-	} else {
-		// if not we take the message as normal
-		msg = message
-		isURL(msg)
-		setID(channelID)
-	};
-	// will listen for messages that will start with `!`
-	if (message.substring(0, 1) == '!') {
-		var args = message.substring(1).split(' ');
-		var cmd = args[0];
-		var tag = args[1]
-		// for playing command, gets whole message after !play
-		var playText = []
-		for(i=1;i<args.length;i++){
-			playText.push(args[i])
-		}
+  // decides whether the data is an upload or a URL message
+  if (message === '' && evt.d.author.bot != true) {
+    // if we get an upload, we look for the attachment URL
+    msg = evt.d.attachments[0].url
+    isURL(msg)
+    setID(channelID)
+  } else {
+    // if not we take the message as normal
+    msg = message
+    isURL(msg)
+    setID(channelID)
+  }
+  // will listen for messages that will start with `!`
+  if (message.substring(0, 1) == '!') {
+    var args = message.substring(1).split(' ')
+    var cmd = args[0]
+    var tag = args[1]
+    // for playing command, gets whole message after !play
+    var playText = []
+    for (i = 1; i < args.length; i++) {
+      playText.push(args[i])
+    }
 
-		args = args.splice(1);
-		switch (cmd) {
-			//commands
-			case 'sauce':
-				console.log(objData)
-				setID(channelID)
-				command.sauce(bot, channelID, objData, lastMsg)
-				break;
-				// a small tag based image search
-			case 'find':
-				command.find(bot, channelID, tag)
-				break;
-			case 'help':
-				command.help(bot, channelID)
-				break;
-			case 'play':
-				activity = playText.join(' ')
-				command.play(bot, activity, userID, channelID)
-				break;
-		}
-	};
+    args = args.splice(1)
+    switch (cmd) {
+      // commands
+      case 'sauce':
+        console.log(objData)
+        setID(channelID)
+        command.sauce(bot, channelID, objData, lastMsg)
+        break
+      // a small tag based image search
+      case 'find':
+        command.find(bot, channelID, tag)
+        break
+      case 'help':
+        command.help(bot, channelID)
+        break
+      case 'play':
+        activity = playText.join(' ')
+        command.play(bot, activity, userID, channelID)
+        break
+      case 'kawaii':
+        command.kawaii(bot, channelID)
+				break
+				// NSFW ******************************
+      case 'lewd':
+        command.lewd(bot, channelID)
+        break
+      case 'feet':
+        command.feet(bot, channelID)
+        break
+      case 'legs':
+        command.legs(bot, channelID)
+        break
+      case 'niisokkusu':
+        command.niisokkusu(bot, channelID)
+        break
+      case 'pantsu':
+        command.pantsu(bot, channelID)
+				break
+			// **************************************
+      case 'th':
+        bot.sendMessage({
+          to: channelID,
+          message: ':thinking:'
+        })
+        break
+      case 'sh':
+        bot.sendMessage({
+          to: channelID,
+          message: '¯' + '\\' + '_(ツ)_/¯'
+        })
+        break
+    }
+  }
+})
+
+bot.on('disconnect', function (err, code) {
+  console.log('Bot disconnected (' + code + ', ' + err + ')')
+  bot.connect()
 })
--- command.js
+++ command.js
@@ -1,11 +1,14 @@
-const Client = require('node-rest-client').Client;
-const client = new Client();
+const Client = require('node-rest-client').Client
+const client = new Client()
+
+var d = new Date()
+var seconds = Math.round(d.getTime() / 1000)
 
 exports.help = function (bot, channelID) {
   bot.sendMessage({
     to: channelID,
-    message: '```!sauce - Use after an image has been posted for other sources of the image.\n !find <tag_name> - Gives you a random image with that tag name **NSFW**\n !play sets the Playing status(admin only)```',
-  });
+    message: '```!sauce - Use after an image has been posted for other sources of the image.\n !find <tag_name> - Gives you a random image with that tag name **NSFW**\n !play sets the Playing status(admin only)```'
+  })
 }
 
 exports.play = function (bot, activity, userID, channelID) {
@@ -15,16 +18,16 @@ exports.play = function (bot, activity, userID, channelID) {
       game: {
         name: activity
       }
-    });
-  }else{
+    })
+  } else {
     bot.sendMessage({
       to: channelID,
-      message: '```You do not have that permission```',
-    });
+      message: '```You do not have that permission```'
+    })
   }
 }
 
-//this is the command for tag searching 
+// this is the command for tag searching 
 
 exports.find = function (bot, channelID, tag) {
   client.get('https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=50&tags=' + tag, function (data, response) {
@@ -32,11 +35,11 @@ exports.find = function (bot, channelID, tag) {
     if (iRes === undefined) {
       bot.sendMessage({
         to: channelID,
-        message: '```No results found```',
-      });
+        message: '```No results found```'
+      })
     } else {
       // set the highest random number to the amount of objs in the res
-      var count = 0;
+      var count = 0
       for (i = 0; i < iRes.length; i++) {
         count = i
         var random = Math.floor(Math.random() * count)
@@ -44,25 +47,25 @@ exports.find = function (bot, channelID, tag) {
       var img = data.posts.post[random].$.file_url
       bot.sendMessage({
         to: channelID,
-        message: img,
-      });
+        message: img
+      })
     }
   })
-  tag = ""
+  tag = ''
 }
 
 // this is the command for image searching
 
 exports.sauce = function (bot, channelID, objData, lastMsg) {
-  var api = 'http://saucenao.com/search.php?db=999&output_type=2&url=';
-  var resultApi = 'http://saucenao.com/search.php?db=999&url=';
+  var api = 'http://saucenao.com/search.php?db=999&output_type=2&url='
+  var resultApi = 'http://saucenao.com/search.php?db=999&url='
 
   // check for anything in the array
   if (lastMsg === undefined) {
     bot.sendMessage({
       to: channelID,
-      message: '```Error, please send a link or upload an image before calling !sauce```',
-    });
+      message: '```Error, please send a link or upload an image before calling !sauce```'
+    })
   } else {
     // calls API 
     client.get(api + objData[channelID], function (data, response) {
@@ -73,27 +76,27 @@ exports.sauce = function (bot, channelID, objData, lastMsg) {
       if (d === undefined) {
         bot.sendMessage({
           to: channelID,
-          message: '```Error, message was not a URL.```',
-        });
+          message: '```Error, message was not a URL.```'
+        })
       } else {
         // all the api calls return different keys, this is how I decided to handle them
         if (d[0].data.eng_name === undefined || d[0].data.eng_name === '') {
           info = d[0].data.title
         } else {
           info = d[0].data.eng_name
-        };
+        }
 
         if (d[0].header.thumbnail === undefined || d[0].header.thumbnail === '') {
           pic = 'https://blog.stylingandroid.com/wp-content/themes/lontano-pro/images/no-image-slide.png'
         } else {
           pic = d[0].header.thumbnail
-        };
+        }
 
         if (d[0].data.creator === undefined || d[0].data.creator === '') {
           author = 'Unknown'
         } else {
           author = d[0].data.creator
-        };
+        }
 
         // sends the embed message with the info
         bot.sendMessage({
@@ -108,8 +111,86 @@ exports.sauce = function (bot, channelID, objData, lastMsg) {
             title: info,
             description: ' \n' + 'Author: ' + author + '\n\n' + '[Saucenao.com Results](' + resultApi + lastMsg + ')'
           }
-        });
-      };
-    });
+        })
+      }
+    })
   }
+}
+
+// redditbooru commands
+
+exports.lewd = function (bot, channelID) {
+  var thing = seconds - (Math.random() * 1000000)
+  var n = 'https://ecchi.redditbooru.com/api/images/?limit=100&nsfw=true&afterDate=' + Math.round(thing)
+  client.get(n, function (data, response) {
+    // parsed response body as js object
+    var nsfw = data[Math.floor(Math.random() * 100)].cdnUrl
+
+    bot.sendMessage({
+      to: channelID,
+      message: nsfw
+    })
+  })
+}
+
+exports.kawaii = function (bot, channelID) {
+  var thing = seconds - (Math.random() * 1000000)
+  var k = 'https://awwnime.redditbooru.com/api/images/?limit=100&nsfw=false&afterDate=' + Math.round(thing)
+  client.get(k, function (data, response) {
+    var kawaii = data[Math.floor(Math.random() * 100)].cdnUrl
+    bot.sendMessage({
+      to: channelID,
+      message: kawaii
+    })
+  })
+}
+
+exports.feet = function (bot, channelID) {
+  var thing = seconds - (Math.random() * 1000000)
+  var n = 'https://animefeet.redditbooru.com/api/images/?limit=100&nsfw=true&afterDate=' + Math.round(thing)
+  client.get(n, function (data, response) {
+    // parsed response body as js object
+    feet = data[Math.floor(Math.random() * 100)].cdnUrl
+
+    bot.sendMessage({
+      to: channelID,
+      message: feet
+    })
+  })
+}
+
+exports.legs = function (bot, channelID) {
+  var thing = seconds - (Math.random() * 1000000)
+  var k = 'https://animelegs.redditbooru.com/api/images/?limit=100&nsfw=false&afterDate=' + Math.round(thing)
+  client.get(k, function (data, response) {
+    var legs = data[Math.floor(Math.random() * 100)].cdnUrl
+    bot.sendMessage({
+      to: channelID,
+      message: legs
+    })
+  })
+}
+
+exports.niisokkusu = function (bot, channelID) {
+  var thing = seconds - (Math.random() * 1000000)
+  var k = 'https://animelegwear.redditbooru.com/api/images/?limit=100&nsfw=false&afterDate=' + Math.round(thing)
+  client.get(k, function (data, response) {
+    var niisokkusu = data[Math.floor(Math.random() * 100)].cdnUrl
+    bot.sendMessage({
+      to: channelID,
+      message: niisokkusu
+    })
+  })
+}
+
+exports.pantsu = function (bot, channelID) {
+  var thing = seconds - (Math.random() * 1000000)
+  var k = 'https://pantsu.redditbooru.com/api/images/?limit=100&nsfw=false&afterDate=' + Math.round(thing)
+  client.get(k, function (data, response) {
+    var pantsu = data[Math.floor(Math.random() * 100)].cdnUrl
+    bot.sendMessage({
+      to: channelID,
+      message: pantsu
+    })
+  })
 }
