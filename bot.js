57dc1107b158e85cf11139d0fadb9d6c605c3054
Jeremy Leitmeyer <jeremyleitmeyer@Alyssas-MacBook-Pro.local>
2018-01-07 23:08:11 -0500

    separated commands

--- bot.js
+++ bot.js
@@ -1,32 +1,14 @@
 const Discord = require('discord.io');
 const logger = require('winston');
 const bodyParser = require('body-parser');
-const Client = require('node-rest-client').Client;
-const client = new Client();
 const reg = require('./regex-weburl.js');
+
+var msg, re_weburl;
+
 
 // variables for eventual api call
 var lastMsg = '';
 var objData = {};
-var api = 'http://saucenao.com/search.php?db=999&output_type=2&url=';
-var resultApi = 'http://saucenao.com/search.php?db=999&url=';
-var msg, re_weburl;
-
-function isURL(msg) {
-	if (reg.re_weburl.test(msg) === false) {
-		return false
-	} else {
-		// clear and push single URL into lastMessage
-		lastMsg = msg
-		return true
-	};
-};
-
-function setID(channelID) {
-	if (isURL(msg)) {
-		objData[channelID] = lastMsg
-	}
-}
 
 //logger settings
 logger.remove(logger.transports.Console);
@@ -53,103 +35,55 @@ bot.on('ready', function (evt) {
 	});
 });
 
+const command = require('./command.js');
+
+function isURL(msg) {
+	if (reg.re_weburl.test(msg) === false) {
+		return false
+	} else {
+		// clear and push single URL into lastMessage
+		lastMsg = msg
+		return true
+	};
+}
+
+function setID(channelID) {
+	if (isURL(msg)) {
+		objData[channelID] = lastMsg
+	}
+}
+
 bot.on('message', function (user, userID, channelID, message, evt) {
 
 	// decides whether the data is an upload or a URL message
 	if (message === '' && evt.d.author.bot != true) {
 		// if we get an upload, we look for the attachment URL
 		msg = evt.d.attachments[0].url
-		setID(channelID)
 		isURL(msg)
+		setID(channelID)
 	} else {
 		// if not we take the message as normal
 		msg = message
-		setID(channelID)
 		isURL(msg)
+		setID(channelID)
 	};
 	// will listen for messages that will start with `!`
 	if (message.substring(0, 1) == '!') {
 		var args = message.substring(1).split(' ');
 		var cmd = args[0];
 		var tag = args[1]
-		var tag2 = args[2]
 
 		args = args.splice(1);
 		switch (cmd) {
 			//commands
 			case 'sauce':
 				console.log(objData)
-				// check for anything in the array
-				if (lastMsg === undefined) {
-					bot.sendMessage({
-						to: channelID,
-						message: 'Error, please send a link or upload an image before calling !sauce',
-					});
-				} else {
-					// calls API 
-					client.get(api + objData[channelID], function (data, response) {
-						var d = data.results
-						var d, info, author, pic
-						// in case !sauce is called without a link
-						if (d === undefined) {
-							bot.sendMessage({
-								to: channelID,
-								message: 'Error, message was not a URL.',
-							});
-						} else {
-							// all the api calls return different keys, this is how I decided to handle them
-							if (d[0].data.eng_name === undefined || d[0].data.eng_name === '') {
-								info = d[0].data.title
-							} else {
-								info = d[0].data.eng_name
-							};
-
-							if (d[0].header.thumbnail === undefined || d[0].header.thumbnail === '') {
-								pic = 'https://blog.stylingandroid.com/wp-content/themes/lontano-pro/images/no-image-slide.png'
-							} else {
-								pic = d[0].header.thumbnail
-							};
-
-							if (d[0].data.creator === undefined || d[0].data.creator === '') {
-								author = 'Unknown'
-							} else {
-								author = d[0].data.creator
-							};
-
-							// sends the embed message with the info
-							bot.sendMessage({
-								to: channelID,
-								message: '',
-								embed: {
-									color: 6826080,
-									thumbnail: {
-										url: pic,
-										width: 200
-									},
-									title: info,
-									description: ' \n' + 'Author: ' + author + '\n\n' + '[Saucenao.com Results](' + resultApi + lastMsg + ')'
-								}
-							});
-						};
-					});
-				}
+				setID(channelID)
+				command.sauce(bot, channelID, objData, lastMsg)
 				break;
-			// a small tag based image search
+				// a small tag based image search
 			case 'find':
-				client.get('https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=50&tags=' + tag, function (data, response) {
-					console.log(data.posts)
-					var iRes = data.posts.post
-					var count = 0;
-					for (i = 0; i < iRes.length; i++) {
-						count = i
-						var random = Math.floor(Math.random() * count)
-					}
-					bot.sendMessage({
-						to: channelID,
-						message: data.posts.post[random].$.file_url,
-					});
-				})
-				tag = ""
+				command.find(bot, channelID, tag)
 				break;
 		}
 	};
---
+++ command.js
@@ -0,0 +1,81 @@
+const Client = require('node-rest-client').Client;
+const client = new Client();
+
+
+exports.find = function (tag, bot, channelID) {
+  client.get('https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=50&tags=' + tag, function (data, response) {
+    var iRes = data.posts.post
+    var count = 0;
+    for (i = 0; i < iRes.length; i++) {
+      count = i
+      var random = Math.floor(Math.random() * count)
+    }
+    var img = data.posts.post[random].$.file_url
+    bot.sendMessage({
+      to: channelID,
+      message: img,
+    });
+  })
+  tag = ""
+}
+
+exports.sauce = function(bot, channelID, objData, lastMsg) {
+  var api = 'http://saucenao.com/search.php?db=999&output_type=2&url=';
+  var resultApi = 'http://saucenao.com/search.php?db=999&url=';
+
+  // check for anything in the array
+  if (lastMsg === undefined) {
+    bot.sendMessage({
+      to: channelID,
+      message: 'Error, please send a link or upload an image before calling !sauce',
+    });
+  } else {
+    // calls API 
+    client.get(api + objData[channelID], function (data, response) {
+      var d = data.results
+      var info, author, pic
+
+      // in case !sauce is called without a link
+      if (d === undefined) {
+        bot.sendMessage({
+          to: channelID,
+          message: 'Error, message was not a URL.',
+        });
+      } else {
+        // all the api calls return different keys, this is how I decided to handle them
+        if (d[0].data.eng_name === undefined || d[0].data.eng_name === '') {
+          info = d[0].data.title
+        } else {
+          info = d[0].data.eng_name
+        };
+
+        if (d[0].header.thumbnail === undefined || d[0].header.thumbnail === '') {
+          pic = 'https://blog.stylingandroid.com/wp-content/themes/lontano-pro/images/no-image-slide.png'
+        } else {
+          pic = d[0].header.thumbnail
+        };
+
+        if (d[0].data.creator === undefined || d[0].data.creator === '') {
+          author = 'Unknown'
+        } else {
+          author = d[0].data.creator
+        };
+
+        // sends the embed message with the info
+        bot.sendMessage({
+          to: channelID,
+          message: '',
+          embed: {
+            color: 6826080,
+            thumbnail: {
+              url: pic,
+              width: 200
+            },
+            title: info,
+            description: ' \n' + 'Author: ' + author + '\n\n' + '[Saucenao.com Results](' + resultApi + lastMsg + ')'
+          }
+        });
+      };
+    });
+  }
+}
