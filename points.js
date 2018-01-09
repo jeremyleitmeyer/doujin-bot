7e0263ab04829f7b84e87fe459e5f3bdec6bbfed
Jeremy Leitmeyer <jeremyleitmeyer@Jeremys-MacBook-Air.local>
2018-01-08 20:59:36 -0500

    point system v1 complete

---
+++ points.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose')
+const Player = require('./models/Player')
+var points, currentUser
+
+exports.new = function (bot, evt, channelID, userID) {
+  console.log(userID)
+  var currentPlayer = Player.findOne({
+    playerId: userID
+  }, function (err, player) {
+    console.log(player)
+    if (player === null) {
+      console.log(player)
+      var newPlayer = new Player({
+        name: evt.d.author.username,
+        playerId: userID,
+        points: 10
+      }).save()
+      currentPlayer = Player.find({
+        playerId: userID
+      }, function (err, player) {
+        console.log(player)
+        bot.sendMessage({
+          to: channelID,
+          message: '```You have been added !```'
+        })
+      })
+    }
+  })
+}
+
+exports.points = function (bot, evt, channelID, userID) {
+  var currentPlayer = Player.findOne({
+    playerId: userID
+  }, function (err, player) {
+    console.log(player)
+    if (player.playerId === userID) {
+      bot.sendMessage({
+        to: channelID,
+        message: player.name + ' has ' + player.points + ' points!'
+      })
+    }
+  })
+}
+
+exports.addOne = function (userID) {
+  console.log('add one')
+  var currentPlayer = Player.findOne({
+    playerId: userID
+  }).update({$inc: {points: 1} }, function (err, doc) {
+    console.log(err)
+  })
+}
+
+exports.addTwo = function (userID) {
+  console.log('add one')
+  var currentPlayer = Player.findOne({
+    playerId: userID
+  }).update({$inc: {points: 2} }, function (err, doc) {
+    console.log(err)
+  })
+}
+
+exports.addThree = function (userID) {
+  console.log('add one')
+  var currentPlayer = Player.findOne({
+    playerId: userID
+  }).update({$inc: {points: 3} }, function (err, doc) {
+    console.log(err)
+  })
+}
