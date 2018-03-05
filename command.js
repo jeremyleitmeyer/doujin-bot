7fbf9829b2bce471dcdc909422bcdbd14d342046
Jeremy Leitmeyer <jeremyleitmeyer@gmail.com>
2018-03-05 17:05:45 -0500

    testing2

--- command.js
+++ command.js
@@ -1,7 +1,6 @@
 const Client = require('node-rest-client').Client
 const client = new Client()
 // this is a test
-// jlskdjf;lkjas;ldkjf;laksjd;flkjals;dkj
 var d = new Date()
 var seconds = Math.round(d.getTime() / 1000)
 
